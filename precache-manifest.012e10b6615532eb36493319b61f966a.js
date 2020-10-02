83ef68c151d6d21a2d09a829fa06d0d9437cbcb0
Soban Venkat <sobanvenkat@Sobans-MBP.domain.name>
2020-10-03 02:08:25 +0530

    Updates

---
+++ precache-manifest.012e10b6615532eb36493319b61f966a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f064f75dae5bc8e874ea6db71127691a",
+    "url": "/facedetection/index.html"
+  },
+  {
+    "revision": "1ebb4d6efee98dbf21f0",
+    "url": "/facedetection/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "61919f27f5638e725864",
+    "url": "/facedetection/static/css/main.8ff6a39c.chunk.css"
+  },
+  {
+    "revision": "1ebb4d6efee98dbf21f0",
+    "url": "/facedetection/static/js/2.00621445.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facedetection/static/js/2.00621445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61919f27f5638e725864",
+    "url": "/facedetection/static/js/main.118e6b50.chunk.js"
+  },
+  {
+    "revision": "99d29fa1a4ca556ebf26",
+    "url": "/facedetection/static/js/runtime-main.7cf075ce.js"
+  }
+]);
